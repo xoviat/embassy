3aa2e59eebc9ebdd44ed9b674f864ec9aa011d00
xoviat <xoviat@users.noreply.github.com>
2025-11-10 19:17:12 -0600

    update example

--- examples/stm32n6/src/bin/blinky.rs
+++ examples/stm32n6/src/bin/blinky.rs
@@ -1,19 +1,32 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::peripheral::NVIC;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_stm32::pac;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Level, Output, Pull, Speed};
+use embassy_stm32::{interrupt, pac, Peri};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+#[embassy_executor::task]
+async fn button_task(mut p: ExtiInput<'static>) {
+    loop {
+        p.wait_for_any_edge().await;
+        info!("button pressed!");
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let mut led = Output::new(p.PB0, Level::High, Speed::Low);
+    let mut led = Output::new(p.PG10, Level::High, Speed::Low);
+    let button = ExtiInput::new(p.PC13, p.EXTI13, Pull::Up);
+
+    spawner.spawn(button_task(button).unwrap());
 
     pac::RCC.misclpenr().modify(|w| {
         w.set_perlpen(true);
@@ -38,16 +51,73 @@ async fn main(_spawner: Spawner) {
         w.set_axisram1lpen(true);
     });
 
+    let irq = interrupt::TIM9;
+
     pac::RCC.apb2lpenr().modify(|w| {
         w.set_tim9lpen(true);
     });
 
+    //    pac::TIM9.dier().modify(|w| w.set_ccie(0, true));
+    pac::TIM9.dier().modify(|w| w.set_ccie(1, true));
+    //
+    //    pac::TIM9.cr1().modify(|w| w.set_cen(false));
+    //    pac::TIM9.cr1().modify(|w| w.set_cen(true));
+
     let mut delay = cortex_m::delay::Delay::new(unsafe { core::mem::transmute(()) }, 64_000_000);
+    let mut nvic = unsafe { cortex_m::Peripherals::steal() }.NVIC;
+
+    let show_status = || {
+        if pac::TIM9.sr().read().uif() {
+            info!("uif set");
+        } else {
+            info!("uif not set");
+        }
+
+        if pac::TIM9.sr().read().ccif(0) {
+            info!("ccif 0 set");
+        } else {
+            info!("ccif 0 not set");
+        }
+
+        if pac::TIM9.sr().read().ccif(1) {
+            info!("ccif 1 set");
+        } else {
+            info!("ccif 1 not set");
+        }
+
+        if NVIC::is_active(irq) {
+            info!("irq active");
+        } else {
+            info!("irq not active");
+        }
+
+        if NVIC::is_enabled(irq) {
+            info!("irq enabled");
+        } else {
+            info!("irq not enabled");
+        }
+
+        if NVIC::is_pending(irq) {
+            info!("irq pending");
+        } else {
+            info!("irq not pending");
+        }
+    };
 
     loop {
+        show_status();
+
         info!("high");
         led.set_high();
         delay.delay_ms(500);
+
+        show_status();
+
+        //        if pac::TIM9.sr().read().ccif(0) {
+        //            info!("ccif 0 set");
+        //        } else {
+        //            info!("ccif 0 not set");
+        //        }
 
         // Timer::after_millis(500).await;
 
